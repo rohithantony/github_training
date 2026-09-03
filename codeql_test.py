674ea2971b980267963d6ff20c090544e66bee94
rohithantony <rohith.aj005@gmail.com>
2026-09-03 15:10:25 +0530

    Refine CodeQL test

--- codeql_test.py
+++ codeql_test.py
@@ -1,5 +1,8 @@
 import subprocess
 
-user_input = input("Enter command: ")
+def main():
+    user_input = input("Enter command: ")
+    subprocess.call(user_input, shell=True)
 
-subprocess.call(user_input, shell=True)
+if __name__ == "__main__":
+    main()
