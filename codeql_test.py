a3f9391a6702fd9750cb77e78f4f1bd5d240b6d0
rohithantony <rohith.aj005@gmail.com>
2026-09-03 15:25:26 +0530

    Update CodeQL test

--- codeql_test.py
+++ codeql_test.py
@@ -1,14 +1,3 @@
-import sqlite3
+user_input = input("Enter Python code: ")
 
-def search_user():
-    username = input("Enter username: ")
-
-    conn = sqlite3.connect("users.db")
-
-    query = "SELECT * FROM users WHERE username = '" + username + "'"
-
-    cursor = conn.execute(query)
-    print(cursor.fetchall())
-
-if __name__ == "__main__":
-    search_user()
+eval(user_input)
